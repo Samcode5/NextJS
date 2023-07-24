8a885e54140bdb95fc37511b4e8f569e15cf9f21
Sameer <gautam.mudawadkar20@vit.edu>
2023-07-24 23:10:09 +0530

    Dashboard changes made , client data fetching swr

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+"use client"
+import useSWR from 'swr'
+import { PageNotFoundError } from 'next/dist/shared/lib/utils';
+import React, { useEffect, useState } from 'react'
 
+
+const  fetcher = (...args) => fetch(...args).then((res) =>  res.json())
  const dashBoard = () => {
+  // useEffect(() =>{
+  //   async function getData(){
+  //     const res= await fetch("https://jsonplaceholder.typicode.com/posts",{cache:"no-store"})
+  //      if(!res.ok)
+  //      {
+  //           return PageNotFoundError;
+  //      }
+    
+  //     const data= await res.json();
+  //     setData(data);
+  //     setIsLoading(false);
+  
+  //   } ;
+  //    getData();
+  // },[])
+  const {data,error} = useSWR('https://jsonplaceholder.typicode.com/posts',fetcher)
+  console.log(data);
   return (
+  
     <div>Dashboard</div>
   )
 }
